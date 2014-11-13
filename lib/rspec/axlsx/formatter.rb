f75a97ebc26dbe78147f3783d39a80719e61a51d
yasuhiroki <yasuhiroki.duck@gmail.com>
2014-11-13 07:38:27 -0800

    cosmetic

--- lib/rspec/axlsx/formatter.rb
+++ lib/rspec/axlsx/formatter.rb
@@ -1,18 +1,16 @@
 require "rspec/axlsx/formatter/version"
 require "rspec/core"
-#require "rspec/core/formatters/base_formatter"
 
 module Rspec
-    #class Formatter < ::RSpec::Core::Formatters::BaseFormatter
-    rspec_version = Gem::Version.new(::RSpec::Core::Version::STRING)
-    rspec_3 = Gem::Version.new('3.0.0')
-    RSPEC_3_AVAILABLE = rspec_version >= rspec_3
+  rspec_version = Gem::Version.new(::RSpec::Core::Version::STRING)
+  rspec_3 = Gem::Version.new('3.0.0')
+  RSPEC_3_AVAILABLE = rspec_version >= rspec_3
 
-    if RSPEC_3_AVAILABLE
-      require "rspec/axlsx/formatter_rspec3"
-      AxlsxFormatter = Rspec::Axlsx::FormatterRSpec3
-    else
-      require "rspec/axlsx/formatter_rspec2"
-      AxlsxFormatter = Rspec::Axlsx::FormatterRSpec2
-    end
+  if RSPEC_3_AVAILABLE
+    require "rspec/axlsx/formatter_rspec3"
+    AxlsxFormatter = Rspec::Axlsx::FormatterRSpec3
+  else
+    require "rspec/axlsx/formatter_rspec2"
+    AxlsxFormatter = Rspec::Axlsx::FormatterRSpec2
+  end
 end
